db1728f910c666a9bd9410bf77b51039e83a20b9
yurifal <fal@izba.com>
2021-09-26 19:13:36 +0300

    Minor fixes.

--- clickhouse_jdbc/connectionProperties.js
+++ clickhouse_jdbc/connectionProperties.js
@@ -14,22 +14,22 @@
     // setting Connection Timeout default value (in milliseconds)
     props["connection_timeout"] = 10000;
 
-	// setting Connection Timeout value (in milliseconds) specified by user
-	if(attr[connectionHelper.attributeVendor1] && attr[connectionHelper.attributeVendor1].length > 0){ 
-	   	props["connection_timeout"] = attr[connectionHelper.attributeVendor1];
-	}
+    // setting Connection Timeout value (in milliseconds) specified by user
+    if(attr[connectionHelper.attributeVendor1] && attr[connectionHelper.attributeVendor1].length > 0){ 
+        props["connection_timeout"] = attr[connectionHelper.attributeVendor1];
+    }
 
-	// setting custom attributes
-	if(attr[connectionHelper.attributeVendor2] && attr[connectionHelper.attributeVendor2].length > 0){
-		var customParams = attr[connectionHelper.attributeVendor2].split(';');
-	    for(var i = 0; i < customParams.length; i++){
-	      var param = customParams[i].split('=');
-		  props[param[0]] = param[1];
-	    }
-	}
+    // setting custom attributes
+    if(attr[connectionHelper.attributeVendor2] && attr[connectionHelper.attributeVendor2].length > 0){
+        var customParams = attr[connectionHelper.attributeVendor2].split(';');
+        for(var i = 0; i < customParams.length; i++){
+          var param = customParams[i].split('=');
+          props[param[0]] = param[1];
+        }
+    }
 
     props["user"] = attr[connectionHelper.attributeUsername];
-    props["password"] = attr[connectionHelper.attributePassword].trim();
+    props["password"] = attr[connectionHelper.attributePassword];
    
     if (attr[connectionHelper.attributeSSLMode] == "require") {
         props["ssl"] = "true";
