cb7297b367bacb7777862e98bf9a7d91b631a426
Artem Shevchenko <9002056+artshevchenko@users.noreply.github.com>
2022-01-14 12:51:20 +0300

    Rewrite Advanced tab and conn string

--- clickhouse_jdbc/connectionProperties.js
+++ clickhouse_jdbc/connectionProperties.js
@@ -1,29 +1,12 @@
 (function propertiesbuilder(attr) {
-    
     var props = {};
 
-    // setting Tableau to use ClickHouse server timezone
-    props["use_server_time_zone"] = 1;
-
-    // setting Tableau to use ClickHouse server timezone for dates
-    props["use_server_time_zone_for_dates"] = 1;
-
-    // setting Socket Timeout default value (in milliseconds)
-    props["socket_timeout"] = 300000;
-
-    // setting Socket Timeout value (in milliseconds) specified by user
-    if(attr["v-socket-timeout"] && attr["v-socket-timeout"].length > 0){ 
-        props["socket_timeout"] = attr["v-socket-timeout"];
-    }
-
-    // setting custom attributes
-    if(attr["v-custom-parameters"] && attr["v-custom-parameters"].length > 0){
-        var customParams = attr["v-custom-parameters"].split(';');
+    // setting custom Connection Parameters
+    if(attr["v-custom-connection-params"] && attr["v-custom-connection-params"].length > 0){
+        var customParams = attr["v-custom-connection-params"].split(',');
         for(var i = 0; i < customParams.length; i++){
             var param = customParams[i].split('=');
-            if(param[0] != "set_session_id"){
-                props[param[0]] = param[1];
-            }
+            props[param[0]] = param[1];
         }
     }
 
